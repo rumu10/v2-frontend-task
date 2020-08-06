cf396d9cec597ca34aef0dc15ac0e56c7c8d5caf
sudipta <sudiptarumu@gmail.com>
2020-08-06 23:52:57 +0600

    Updates

---
+++ precache-manifest.c4023b4a48d5c0e0eecdd925637e4b1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ca8767bb79896a5bffa867f575f252",
+    "url": "/v2-frontend-task/index.html"
+  },
+  {
+    "revision": "edc3e126f259c2e8f909",
+    "url": "/v2-frontend-task/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47dd32bbe823c128e54d",
+    "url": "/v2-frontend-task/static/js/2.ce71dadf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/v2-frontend-task/static/js/2.ce71dadf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc3e126f259c2e8f909",
+    "url": "/v2-frontend-task/static/js/main.f4b500c5.chunk.js"
+  },
+  {
+    "revision": "286a449fa36063f3f1ae",
+    "url": "/v2-frontend-task/static/js/runtime-main.487b5a73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/v2-frontend-task/static/media/logo.5d5d9eef.svg"
+  }
+]);
