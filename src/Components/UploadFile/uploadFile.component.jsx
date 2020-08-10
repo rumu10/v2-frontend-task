c27854b1c20f3159052df3fbb411a44868a1cbbb
sudipta <sudiptarumu@gmail.com>
2020-08-10 20:18:57 +0600

    file uploading fucntionality done

--- src/Components/UploadFile/uploadFile.component.jsx
+++ src/Components/UploadFile/uploadFile.component.jsx
@@ -1,31 +1,110 @@
 import React, { Component } from 'react';
-import img from '../../Asset/img_2.jpg';
-import img2 from '../../Asset/img_3.jpg';
+import Dropzone from "react-dropzone";
+
+import pdf from '../../Asset/pdf.png';
+import msword from '../../Asset/msword.png';
+import msexcel from '../../Asset/msexcel.png';
+import otherfile from '../../Asset/file.png';
 
 import './uploadFile.style.scss';
 
 class UploadFile extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            file_list: []
+        }
+    };
+
+    onFileUpload = (acceptedFiles ,draggedFiles) => {   //Method for adding all files in a list on upload or on drag
+        let file_list = this.state.file_list;
+        let files = acceptedFiles;
+        let draged_files=draggedFiles;
+        let i = 0;
+        while (i < files.length) {
+            file_list.push(files[i]);
+            i++;
+        }
+        while (i < draged_files.length) {
+            file_list.push(files[i]);
+            i++;
+        }
+        this.setState({
+            file_list: file_list
+        })
+    };
+
+    onFileRemove = (fileName) => {  //method for removinf the uploaded files
+        let file_list = this.state.file_list;
+        file_list = file_list.filter((file) => { return file.name !== fileName.name });
+        this.setState({
+            file_list: file_list
+        })
+    };
+
+    get_file_extension = (fileName) => {    //method for getting different file extention
+        let ext = '';
+        let splitted = fileName.split('.');
+        if (splitted.length > 1) {
+            ext = splitted[splitted.length - 1];
+        }
+        return ext;
+    };
+
+    file_to_thumbnail = (file) => {     //method for showing thumbnails of iploaded files
+        let ext = this.get_file_extension(file['name']).toLowerCase();
+        const imgtypes = ['png', 'jpg', 'jpeg', 'bmp'];
+        const pdftypes = ['pdf'];
+        const xltypes = ['xls', 'xlsx', 'xlx', 'csv'];
+        const doctypes = ['doc', 'docx', 'odt'];
+        if (imgtypes.indexOf(ext) !== -1) {
+            if (file['id']) {
+                return file['name'];
+            } else {
+                return window.webkitURL.createObjectURL(file);
+            }
+        } else if (pdftypes.indexOf(ext) !== -1) {
+            return pdf;
+        } else if (xltypes.indexOf(ext) !== -1) {
+            return msexcel;
+        } else if (doctypes.indexOf(ext) !== -1) {
+            return msword;
+        } else {
+            return otherfile;
+        }
+    };
+
     render() {
         return (
             <div className="upload">
                 <h6>Upload Attachment</h6>
                 <div className="up_box">
-                    <div className="text-center">Drop your Ataachment here or Click to Upload</div>
-                    <i className="fa fa-cloud-upload up_icon" aria-hidden="true"></i>
+                    <Dropzone onDrop={this.onFileUpload}>
+                        {({ getRootProps, getInputProps }) => (
+                            <div {...getRootProps({ className: "dropzone" })}>
+                                <input {...getInputProps()} />
+                                <p>Drag'n'drop files, or click to select files</p>
+                                <i className="fa fa-cloud-upload up_icon" aria-hidden="true"></i>
+                            </div>
+                        )}
+                    </Dropzone>
+
                 </div>
                 <div className="row uploaded_files">
-                    <div className="col-3">
-                        <img className="" src={img} width="80" height="80" />
-                    </div>
-                    <div className="col-3">
-                        <img className="" src={img2} width="80" height="80" />
-                    </div>
-                    <div className="col-3">
-                        <img className="" src={img} width="80" height="80" />
-                    </div>
-                    <div className="col-3">
-                        <img className="" src={img2} width="80" height="80" />
-                    </div>
+                    {this.state.file_list && [...this.state.file_list].map((file, i) => (
+                        <div className="col-md-3" key={i}>
+                            <div className="thumbnail">
+                                <button
+                                    type="button"
+                                    className="close"
+                                    style={{ marginBottom: -20, }}
+                                    onClick={this.onFileRemove.bind(this, file)}>×</button>
+                                <img src={this.file_to_thumbnail(file)} alt="" width="80" height="80" />
+                                <p className="file_name">{file['name']}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         )
